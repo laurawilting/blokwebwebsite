74826596b9ab100040d3d471cf53d5ab486e0482
laurawilting <118051135+laurawilting@users.noreply.github.com>
2022-12-09 12:42:13 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+const deMenuButton = document.querySelector("header button");
+const deNav = document.querySelector("header nav");
+
+deMenuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	deNav.classList.toggle("open");
+}
+
